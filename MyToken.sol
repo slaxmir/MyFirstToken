38066290806e1e71338e5111508b032848936a24
slaxmir <slaxmi.rajaboina@gmail.com>
2023-05-16 16:43:25 +0530

    MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.8.15;
 
 /**
 Demo Assement
-1.Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., 
+1.The Contract will have public variables that store the details about your coin (Token Name, Token Abbrv., 
   Total Supply)
-2.Your contract will have a mapping of addresses to balances (address => uint)
+2.The Contract will have a mapping of addresses to balances (address => uint)
    You will have a mint function that takes two parameters: an address and a value. 
 3.The function then increases the total supply by that number and increases the balance of the address by that amount.
-4.Your contract will have a burn function, which works the opposite of the mint function, 
+4.The Contract will have a burn function, which works the opposite of the mint function, 
   as it will destroy tokens. It will take an address and value just like the mint functions. 
   It will then deduct the value from the total supply and from the balance of the address.
 5.Lastly, your burn function should have conditionals to make sure the balance of account 
